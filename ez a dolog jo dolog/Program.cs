770d04a848a53d3523209b18e341c8d7a92a6aeb
Pzsombor17 <Pap.Zsombor@C19-01>
2026-09-24 08:44:24 +0200

    record student osztaly

--- ez a dolog jo dolog/Program.cs
+++ ez a dolog jo dolog/Program.cs
@@ -8,6 +8,8 @@
             Book book = new Book("Hobbit", "gandalf jeno", 310) {Price = 65000 };
             book.UpdatePrice(10);
             Console.WriteLine(book);
+            Book dune = book with { Author = "kjhb", Title = "dfs", Pages = 444 };
+            Console.WriteLine(dune);
         }
     }
 }
---
+++ ez a dolog jo dolog/student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ez_a_dolog_jo_dolog
+{
+    internal record student(string name, string classname,int BirthYear)
+    {
+        private double _avarage { get; set; }
+    }
+}
