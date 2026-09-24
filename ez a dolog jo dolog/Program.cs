3e1694c00f6dc15fc0ce7e554e9a8d779a415e94
Pzsombor17 <Pap.Zsombor@C19-01>
2026-09-24 08:40:59 +0200

    Add project files.

---
+++ ez a dolog jo dolog/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ez_a_dolog_jo_dolog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Book book = new Book("Hobbit", "gandalf jeno", 310) {Price = 65000 };
+            book.UpdatePrice(10);
+            Console.WriteLine(book);
+        }
+    }
+}
---
+++ ez a dolog jo dolog/book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ez_a_dolog_jo_dolog
+{
+  internal record Book(string Title,string Author,int Pages)
+    {
+        public double Price { get; set; }
+        public void UpdatePrice(double percent)
+        {
+            Price *= (100 - percent) / 100;
+        }
+            public double GetPrice3()
+        {
+            return Price * 3;
+        }
+        
+    }
+}
